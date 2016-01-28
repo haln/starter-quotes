1e8989ebe0da10bed7a8195c635ce66d25b31bdd
Kreygasm <anderson0phan@gmail.com>
2016-01-28 12:54:13 -0800

    class renamed
    removed previous last.php in controllers and fixed the subfolder
    controller

--- application/controllers/last/Welcome.php
+++ application/controllers/last/Welcome.php
@@ -2,11 +2,11 @@
 	/**
 	 * the last page with the rendered quote.
 	 *
-	 * controllers/Last.php
+	 * controllers/last/Welcome.php
 	 *
 	 * ------------------------------------------------------------------------
 	 */
-class Last extends Application {
+class Welcome extends Application {
 
 	function __construct()
 	{
