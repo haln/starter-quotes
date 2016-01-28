f3fcfa91c39f3c58a5e94e0927b7d5cc2b426330
Justin Walk <justinwalk@live.com>
2016-01-28 12:55:33 -0800

    change to justone view

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -28,7 +28,7 @@ class First extends Application{
 
   function zzz()
   {
-      $this->data['pagebody'] = 'first';	// this is the view we want shown
+      $this->data['pagebody'] = 'justone';	// this is the view we want shown
       $record = $this->quotes->get(1);
       $this->data = array_merge($this->data, $record);
       $this->render();
